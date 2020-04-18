89fccd7c7a090351afd1516e1f5770cd80fd0acf
navo1997 <53464385+navo1997@users.noreply.github.com>
2020-04-18 20:12:06 +0530

    Patient class added

---
+++ PatientManagementNew/src/main/java/com/project/healthcarepatient/PatientManagementNew/Patient.java
@@ -0,0 +1,73 @@
+package com.project.healthcarepatient.PatientManagementNew;
+
+public class Patient {
+	private int pid;
+	private String nic;
+	private String fname;
+	private String lname;
+	private int age;
+	private String address;
+	private String gender;
+	private String email;
+	private int phoneNo;
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public String getNic() {
+		return nic;
+	}
+	public void setNic(String nic) {
+		this.nic = nic;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(int phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	@Override
+	public String toString() {
+		return "Patient [pid=" + pid + ", nic=" + nic + ", fname=" + fname + ", lname=" + lname + ", age=" + age
+				+ ", address=" + address + ", gender=" + gender + ", email=" + email + ", phoneNo=" + phoneNo + "]";
+	}
+	
+}
